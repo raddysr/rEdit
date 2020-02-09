c6fa29b3454a53f33f6bd24a55df4c589f42c58a
rrraddy <raddy.radev@gmail.com>
2020-02-09 22:02:28 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+import cx_Freeze
+import sys
+import os
+base = None
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+os.environ['TCL_LIBRARY'] =r"C:\Python\Python37\tcl\tcl8.6"
+os.environ['TK_LIBRARY'] =r"C:\Python\Python37\tcl\tk8.6"
+
+executables = [cx_Freeze.Executable("rEdit.py", base=base, icon="icon.ico")]
+
+cx_Freeze.setup(
+    name = "rEdit Text Editor",
+    options = {"build_exe": {"packages":["tkinter","os"],"include_files":["icon.ico",'tcl86t.dll','tk86t.dll', 'icons']}},
+    version = "0.01",
+    description = "Tkinter Application",
+    executables = executables
+)
